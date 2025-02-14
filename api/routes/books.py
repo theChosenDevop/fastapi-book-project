90524f92d334162feddcb6c753b3f9f123324079
theChosenDevop <tobbey.adesanya@gmail.com>
2025-02-14 19:14:49 +0100

    Add AmazingFeature

--- api/routes/books.py
+++ api/routes/books.py
@@ -62,3 +62,10 @@ async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    if book_id not in db.books:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"})
+    return db.get_book(book_id).model_dump()
